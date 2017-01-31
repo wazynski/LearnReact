e8defbb8e1826388995e87721c93051ead18cb0c
Tom Skarbek-Wazynski <wazynski@gmail.com>
2017-01-31 19:14:52 +0000

    Learn't about state

--- VideoApp/src/components/search_bar.js
+++ VideoApp/src/components/search_bar.js
@@ -9,15 +9,27 @@ import React, { Component } from 'react';
 
 // class component must always render some JSX is self aware
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
   render() {
     // You can define a callback using this format and the defin that in the class
     // return <input onChange={this.onInputChange} />;
 
     // Or, you can run the code directly using a ES6 funcion
-    return <input onChange={event => console.log(event.target.value)} />;
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={event => this.setState({ term : event.target.value })} />
+      </div>
+    )
   }
 
-  // This is the callback that is called onChange in the commented out example 
+  // This is the callback that is called onChange in the commented out example
   // onInputChange(event) {
   //   console.log(event.target.value);
   // }
