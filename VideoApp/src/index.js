9d6eeef7ccf9b02b46ac0a0de8f1623f2dfbe7c9
Tom Skarbek Wazynski <t.skarbek-wazynski@lancaster.ac.uk>
2017-02-01 08:56:29 +0000

    Video detail and player added. Video list added.

--- VideoApp/src/index.js
+++ VideoApp/src/index.js
@@ -1,22 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
+// Downwards data flow. The most parent component should be resposible for fetching data.
 const API_KEY = 'AIzaSyBDLui5HjS-REv6U2ovpk8SWy7czeKUY6Y';
-
-// Create new component. This component should produe some HTML
 
 // const is ES6 is simlar to var but it will never chnage it is a constant
 // JSX allows for html in javascript, it looks like HTML but is really JS.
 // ES6 () => {} is equal to function() {} however scop of this changes slightly
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: []
+    };
+
+    YTSearch({key :API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    )
+  }
 };
 
 // Take this component's generated HTML and put it on the page (DOM)
