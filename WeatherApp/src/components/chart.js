509158bdde431a2cfd88e2ba67b50c570df65e64
Tom Skarbek Wazynski <t.skarbek-wazynski@lancaster.ac.uk>
2017-02-02 13:48:11 +0000

    Added google maps support

--- WeatherApp/src/components/chart.js
+++ WeatherApp/src/components/chart.js
@@ -17,5 +17,3 @@ export default (props) => {
     </div>
   )
 }
-
-// test
--- WeatherApp/src/containers/weather-list.js
+++ WeatherApp/src/containers/weather-list.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google-map';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
     const temps = cityData.list.map(weather => weather.main.temp)
     const humidities = cityData.list.map(weather => weather.main.pressure)
     const pressures = cityData.list.map(weather => weather.main.humidity)
-
+    const {lon, lat} = cityData.city.coord;
     return(
       <tr key={cityData.city.id}>
-        <td>{cityData.city.name}</td>
+        <td><GoogleMap lon={lon} lat={lat}/></td>
         <td><Chart data={temps} color="red" units="K"/></td>
         <td><Chart data={pressures} color="orange" units="HPA"/></td>
         <td><Chart data={humidities} color="blue" units="%"/></td>
