1161be52557331c66ea62570a6a92ca1b95b10cc
Tom Skarbek Wazynski <t.skarbek-wazynski@lancaster.ac.uk>
2017-02-21 09:29:10 +0000

    Finished off blog app

--- BlogApp/src/components/posts_new.js
+++ BlogApp/src/components/posts_new.js
@@ -1,16 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsNew extends Component {
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
+  onSubmit(props) {
+    // createPost is an action and returbns a promise.
+    this.props.createPost(props)
+      .then(() => {
+        // Blog post has been created navigate to the IndexRoute
+        // We navigate by calling this.context.router.push with the
+        //new path to navigate to.
+        this.context.router.push('/');
+      });
+  }
+
   render() {
     const { fields: { title, categories, content }, handleSubmit } = this.props;
 
     return(
-      <form onSubmit={handleSubmit(this.props.createPost)}>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <h3>Create a New Post</h3>
 
-        <div className="form-group">
+        <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
           <label>Title</label>
           <input type="text" className="form-control" {...title} />
           <div className="text-help">
@@ -18,7 +34,7 @@ class PostsNew extends Component {
           </div>
         </div>
 
-        <div className="form-group">
+        <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
           <label>Categories</label>
           <input type="text" className="form-control" {...categories} />
           <div className="text-help">
@@ -26,7 +42,7 @@ class PostsNew extends Component {
           </div>
         </div>
 
-        <div className="form-group">
+        <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
           <label>Content</label>
           <textarea rows="10" className="form-control" {...content} />
           <div className="text-help">
@@ -35,6 +51,7 @@ class PostsNew extends Component {
         </div>
 
         <button className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
       </form>
     );
   }
